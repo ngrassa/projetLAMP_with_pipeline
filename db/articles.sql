a5447695c8051e252304bf8f80113174bb4a8a47
Noureddine GRASSA <ngrassa@gmail.com>
2026-03-18 00:15:22 +0100

    Modif des articles

--- db/articles.sql
+++ db/articles.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS articles (
 );
 
 INSERT INTO articles (titre, contenu) VALUES
-('Article 1', 'Contenu du premier article'),
-('Article 2', 'Contenu du deuxième article'),
-('Article 3', 'Contenu du troisième article');
+('Article 1', 'Cloud Computing'),
+('Article 2', 'DevSecOps'),
+('Article 3', 'Gitops');
